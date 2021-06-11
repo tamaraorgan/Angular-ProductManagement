8a69b0f6efcc86acc5d842d12563e04ec9ed3130
Tamara Organ <tamara@organ.dev.br>
2021-06-11 16:23:35 -0400

    create - product list

---
+++ src/app/components/product/product-list/product-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements OnInit {
+  pageTitle = 'Products List';
+  showImage = false;
+  products: any[] = [
+    {
+      productId: 2,
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2021',
+      description: '15 gallon capacity rolling garden cart',
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl: 'assets/images/garden_cart.png',
+    },
+    {
+      productId: 5,
+      productName: 'Hammer',
+      productCode: 'TBX-0048',
+      releaseDate: 'May 21, 2021',
+      description: 'Curved claw steel hammer',
+      price: 8.99,
+      starRating: 4.8,
+      imageUrl: 'assets/images/hammer.png',
+    },
+  ];
+
+  toggleImage() {
+    this.showImage = !this.showImage
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
+}
